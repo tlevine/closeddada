62d7986107e532f168b02be250c41f007dcf947c
Thomas Levine <_@thomaslevine.com>
2014-10-02 21:49:55 +0000

    -

--- datamarts/notmuch/parse_addresses.py
+++ datamarts/notmuch/parse_addresses.py
@@ -1,17 +1,25 @@
 import pyparsing as p
 
 # Name part of a person
-unquoted_name = p.Combine(p.OneOrMore(p.Word(''.join(set(p.printables) - {'<'}))))
-quoted_name = p.Group('"' + unquoted_name + p.Suppress('"'))
+chars = ''.join(set(p.printables) - set('"<>'))
+unquoted_name = p.Combine(p.OneOrMore(p.Word(chars)))
+quoted_name = p.Suppress('"') + unquoted_name + p.Suppress('"')
 name = p.Or([unquoted_name, quoted_name])
 
 # Address itself
-address_only = p.Word(p.printables)
+address_only = p.Word(chars)
 
 # Combination of name and address
 address = p.Or([
-    name + p.Suppress('<') + address_only + p.Suppress('>'),
-    address_only])
+    address_only,
+    name + p.Suppress(p.Optional('<')) + address_only + p.Suppress('>')
+])
 
 # Many addresses
-addresses = p.OneOrMore(address)
+addresses = p.OneOrMore(p.Group(address))
+
+def parse_addresses(raw):
+    for parsed in addresses.parseString(raw):
+        name = ' '.join(parsed[:-1])
+        address = parsed[-1]
+        yield name, address
--- datamarts/notmuch/test/test_parse_addresses.py
+++ datamarts/notmuch/test/test_parse_addresses.py
@@ -1,14 +1,18 @@
 import nose.tools as n
 
-from ..parse_addresses import addresses
+from ..parse_addresses import parse_addresses
 
 TESTCASES = [
     ('Thomas Levine <road-bike@thomaslevine.com>',
         [('Thomas Levine', 'road-bike@thomaslevine.com')]),
+    ('"Thomas Levine" <road-bike@thomaslevine.com>',
+        [('Thomas Levine', 'road-bike@thomaslevine.com')]),
+    ('public@thomaslevine.com',
+        [(None, 'public@thomaslevine.com')]),
 ]
 
 def check(raw, parsed):
-    n.assert_list_equal(list(addresses.parseString(raw)), parsed)
+    n.assert_list_equal(list(parse_addresses(raw)), parsed)
 
 def test():
     for raw, parsed in TESTCASES:
